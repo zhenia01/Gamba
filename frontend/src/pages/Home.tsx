11c78f9f11f0114fd84886d328efc2f650c373ed
zhenia01 <yevhen.borodaykevich@gmail.com>
2023-02-07 15:10:38 +0200

    Enhance home page for quick endpoint access

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,23 @@
 import { Link } from 'react-router-dom';
 
-import { useCurrentUser } from '@/features/auth';
+import { authActions, useCurrentUser } from '@/features/auth';
 
 function Home() {
   const user = useCurrentUser();
 
-  return(
-  <div>
-    <p>Hello, {user?.name ?? 'anonymous'}</p>
-    {!user && <Link to="/sign-up" >Sign Up</Link>}
-  </div>);
+  return (
+    <div>
+      <p>Hello, {user?.name ?? 'anonymous'}</p>
+      <div>
+        {!user && <Link to="/sign-in">Sign In</Link>}
+      </div>
+      <div>
+        {!user && <Link to="/sign-up">Sign Up</Link>}
+      </div>
+      <div>
+        {user && <button onClick={authActions.signOut}>Sign out</button>}
+      </div>
+    </div>);
 }
 
 export { Home };
