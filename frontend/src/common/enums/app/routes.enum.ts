907d2ec6b6387f5f099c85d5bf193284e0cf7803
zhenia01 <yevhen.borodaykevich@gmail.com>
2023-02-12 22:46:18 +0200

    FE-11 Define enum for string routes

---
+++ frontend/src/common/enums/app/routes.enum.ts
@@ -0,0 +1,10 @@
+const enum AppRoute {
+  ROOT = '/',
+  SIGN_IN = '/sign-in',
+  SIGN_UP = '/sign-up',
+  ANY = '*',
+  HOME = '/home',
+  DASHBOARD = '/dashboard',
+}
+
+export { AppRoute };
--- frontend/src/common/enums/index.ts
+++ frontend/src/common/enums/index.ts
@@ -1,3 +1,4 @@
 export { ENV } from './app/env.enum';
+export { AppRoute } from './app/routes.enum';
 export type { ContentType } from './http/content-type';
 export { HttpStatusCode } from './http/http-code.enum';
