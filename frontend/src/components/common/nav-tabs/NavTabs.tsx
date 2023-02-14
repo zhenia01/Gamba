42045721de8745d7a480eb1d9b15128f9fdbfa6c
Roman <romaniv788@gmail.com>
2023-02-14 15:54:36 +0200

    simplify code, add more specific styles to remove box shadow from tab

--- frontend/src/components/common/nav-tabs/NavTabs.tsx
+++ frontend/src/components/common/nav-tabs/NavTabs.tsx
@@ -1,10 +1,7 @@
 import './NavTabs.scss';
 
 import { Tab, TabList, TabPanels, Tabs } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
-
-import { noop } from '@/common/utils/noop';
 
 import type { NavTabData } from './interfaces/nav-tab-data';
 import { getCurrentTabIndex } from './utils/get-current-tab-index';
@@ -14,16 +11,11 @@ type Props = {
 };
 
 export function NavTabs({ data }: Props) {
-  const [currentTabIndex, setCurrentTabIndex] = useState<number>();
   const location = useLocation();
-
-  useEffect(() => {
-    const tabIndex = getCurrentTabIndex(location.pathname, data);
-    setCurrentTabIndex(tabIndex);
-  }, [location]);
+  const currentTabIndex = getCurrentTabIndex(location.pathname, data);
 
   return (
-    <Tabs index={currentTabIndex} onChange={noop}>
+    <Tabs index={currentTabIndex}>
       <TabList>
         {data.map(({ label, index, path }) => (
           <Tab as={Link} to={path} key={index}>
