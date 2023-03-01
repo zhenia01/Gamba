f23cf99c51a9c860812ec78b5f3e7ec60a99de52
Roman <romaniv788@gmail.com>
2023-03-01 15:10:37 +0200

    Revert changes with moving validation logic to react hook form onl

--- frontend/src/features/auth/common/validations/password.validation.ts
+++ frontend/src/features/auth/common/validations/password.validation.ts
@@ -1,4 +1,4 @@
 export const passwordValidation = {
-  minLength: { value: 5, message: 'Please enter more than 5 characters' },
-  required: 'This field is required',
+  minLength: 5,
+  required: true,
 };
