42d30067ca28d9d6fb884286ad56312ddfa957a8
Roman <romaniv788@gmail.com>
2023-03-02 18:17:29 +0200

    Simplify error message and condition for http error codes

--- frontend/src/store/middlewares/action-middlewares/handle-error.ts
+++ frontend/src/store/middlewares/action-middlewares/handle-error.ts
@@ -1,4 +1,4 @@
-import { HttpStatusCode } from '@/common/enums';
+import { HttpError } from '@/common/types';
 import { openToast } from '@/components/common';
 import { applyProxyOnActions } from '@/store/middlewares/action-middlewares/common/apply-proxy';
 import { StoreActions } from '@/store/middlewares/action-middlewares/common/store-actions.type';
@@ -9,15 +9,14 @@ const handleError = <T extends StoreActions>(actions: T): T =>
       await action();
     } catch (e) {
       if (
-        e instanceof Error &&
-        e.message.includes(HttpStatusCode.INTERNAL_SERVER_ERROR.toString())
+        e instanceof HttpError &&
+        e.details.status >= 500 &&
+        e.details.status <= 599
       ) {
         openToast({
-          title: e.name,
-          description: e.message,
+          title: e.details.title,
+          description: e.details.detail,
           status: 'error',
-          duration: 9000,
-          isClosable: true,
         });
       }
       throw e;
