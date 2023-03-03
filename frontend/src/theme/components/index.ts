925f0b5ac9280d7e9a0580ac50751a1a12346a64
zhenia01 <yevhen.borodaykevich@gmail.com>
2023-03-03 16:31:07 +0200

    FE: Add styles for sign-in, sign-up

--- frontend/src/theme/components/index.ts
+++ frontend/src/theme/components/index.ts
@@ -1,5 +1,7 @@
-import { FormError } from './form-error';
+import { Form } from './form.style-config';
+import { FormError } from './form-error.style-config';
 
 export const components = {
   FormError,
+  Form,
 };
