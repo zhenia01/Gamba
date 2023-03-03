6289a667bce6df6a3744edfdb52955dd9e835ab1
zhenia01 <yevhen.borodaykevich@gmail.com>
2023-03-03 19:29:44 +0200

    FE: remove unnecessary index files

--- frontend/src/routes/home/home.route.tsx
+++ frontend/src/routes/home/home.route.tsx
@@ -2,7 +2,7 @@ import { NonIndexRouteObject } from 'react-router-dom';
 
 import { AppRoute } from '@/common/enums';
 import { ProtectedRoute } from '@/components/common';
-import { Home } from '@/pages';
+import { Home } from '@/pages/Home';
 
 const homeRoute: NonIndexRouteObject = {
   path: AppRoute.HOME,
