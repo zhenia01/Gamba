f5839720468d3f374f19870c6c407544a3e7094a
Roman <romaniv788@gmail.com>
2023-03-07 18:12:32 +0200

    Add port to FE enviroment variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,26 @@
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
 // eslint-disable-next-line import/no-default-export
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    watch: {
-      usePolling: true,
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: +env.VITE_PORT || 80,
     },
-    host: true,
-    strictPort: true,
-    port: 80,
-  },
+  };
 });
