2086ff9b594d415da9d7d8ffef50f2c59799d04c
zhenia01 <yevhen.borodaykevich@gmail.com>
2023-04-26 17:14:49 +0300

    BE: add tags commands and queries

---
+++ backend/Gamba.Application/Users/Tags/UpdateTags/AddFavoriteTags/AddFavoriteTagsCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace Gamba.Application.Users.Tags.UpdateTags.AddFavoriteTags;
+
+public record AddFavoriteTagsCommand(Guid Id, IEnumerable<string> Tags) : UpdateTagsCommandBase(Id, Tags);
---
+++ backend/Gamba.Application/Users/Tags/UpdateTags/UpdateTagsCommandHandlerBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+using Gamba.Application.Configuration.Commands;
+using Gamba.Application.Exceptions;
+using Gamba.Application.Users.Tags.UpdateTags.AddFavoriteTags;
+using Gamba.Domain.Users;
+using Gamba.Domain.Users.Tags;
+using Gamba.Infrastructure.Domain.Users;
+
+namespace Gamba.Application.Users.Tags.UpdateTags;
+
+public abstract class UpdateTagsCommandHandlerBase<TCommand> : ICommandHandler<TCommand, List<Tag>>
+    where TCommand: UpdateTagsCommandBase
+{
+    private readonly UserRepository _userRepository;
+
+    protected UpdateTagsCommandHandlerBase(
+        UserRepository userRepository
+    )
+    {
+        _userRepository = userRepository;
+    }
+
+    protected abstract void TagsAction(User user, IEnumerable<Tag> tags);
+    protected abstract List<Tag> ResultTagsSelector(User user);
+
+    public async Task<List<Tag>> Handle(TCommand request, CancellationToken cancellationToken)
+    {
+        var (userId, tagsToAdd) = request;
+        var user = await _userRepository.GetById(new UserId(userId)) ?? throw new EntityNotFoundException("User's not found");
+        
+        TagsAction(user, tagsToAdd.Select(t => new Tag(t)));
+
+        await _userRepository.SaveChanges();
+
+        return ResultTagsSelector(user);
+    }
+}
